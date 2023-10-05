e08d5f4d8c1c67171ce0f6036b4c4e464f3b9f6c
TimurAztec <timotiaztec@gmail.com>
2023-10-05 19:21:21 +0300

    Fixed task 2

--- Lection 1/task 2/main.py
+++ Lection 1/task 2/main.py
@@ -1,5 +1,3 @@
-tries_count: int = 0
-
 def is_int_convertable(val: str):
     try: 
         int(val)
@@ -10,10 +8,7 @@ def is_int_convertable(val: str):
 
 def sum_up(val1: str, val2: str):
     if (not is_int_convertable(val1)) or (not is_int_convertable(val2)):
-        global tries_count
-        tries_count += 1
-        print("Please enter corrent numbers!") if tries_count <= 5 else print("Ты шо дебил? Ты в школе числа не учил? Числа вводи нормально!")
-        if tries_count > 5: tries_count = 0
+        print("Please enter corrent numbers!")
         return
     print(f"Numbers sum is: {int(val1) + int(val2)}")
 
