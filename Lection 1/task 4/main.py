0a261d5a95c3c94002e03fe6f2f49332836d276b
TimurAztec <timotiaztec@gmail.com>
2023-10-05 20:11:07 +0300

    Added task 4

--- Lection 1/task 4/main.py
+++ Lection 1/task 4/main.py
@@ -1,10 +1,27 @@
-def is_int_convertable(val: str):
-    try: 
-        int(val)
-    except ValueError:
-        return False
-    else:
-        return True
-    
-    if __name__ == "__main__":
-        
+import math
+
+action = input("Enter math action you wanna perform ( + , - , *, / , sqrt, ^ ): ")
+match action:
+    case "+":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        print(f"Answer {num1 + num2}")
+    case "-":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        print(f"Answer {num1 - num2}")
+    case "*":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        print(f"Answer {num1 * num2}")
+    case "/":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        print(f"Answer {num1 / num2}")
+    case "sqrt":
+        num = int(input("Enter number: "))
+        print(f"Answer {math.sqrt(num)}")
+    case "^":
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        print(f"Answer {num1**num2}")
