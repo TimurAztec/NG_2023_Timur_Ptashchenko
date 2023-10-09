105e8ed4f5726a64bf7d441f73da76b6b3c1f7c5
TimurAztec <timotiaztec@gmail.com>
2023-10-09 21:32:33 +0300

    Updated task 4

--- Lection 1/task 4/main.py
+++ Lection 1/task 4/main.py
@@ -1,27 +1,21 @@
 import math
 
 action = input("Enter math action you wanna perform ( + , - , *, / , sqrt, ^ ): ")
+if (action == "sqrt"):
+    num1 = int(input("Enter number: "))
+else:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 match action:
     case "+":
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
         print(f"Answer {num1 + num2}")
     case "-":
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
         print(f"Answer {num1 - num2}")
     case "*":
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
         print(f"Answer {num1 * num2}")
     case "/":
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
         print(f"Answer {num1 / num2}")
     case "sqrt":
-        num = int(input("Enter number: "))
-        print(f"Answer {math.sqrt(num)}")
+        print(f"Answer {math.sqrt(num1)}")
     case "^":
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
         print(f"Answer {num1**num2}")
