a0c8516d40881014eb55960e33edae841a1833eb
TimurAztec <timotiaztec@gmail.com>
2023-10-09 21:28:21 +0300

    Made task 2 simpler

--- Lection 1/task 2/main.py
+++ Lection 1/task 2/main.py
@@ -1,18 +1,4 @@
-def is_int_convertable(val: str):
-    try: 
-        int(val)
-    except ValueError:
-        return False
-    else:
-        return True
-
-def sum_up(val1: str, val2: str):
-    if (not is_int_convertable(val1)) or (not is_int_convertable(val2)):
-        print("Please enter corrent numbers!")
-        return
-    print(f"Numbers sum is: {int(val1) + int(val2)}")
-
-if __name__ == "__main__":
-    while True:
-        print("Enter 2 numbers to sum up:")
-        sum_up(input("Enter first number: "), input("Enter second number: "))
+print("Enter 2 numbers to sum up:")
+val1 = input("Enter first number: ")
+val2 = input("Enter second number: ")
+print(f"Numbers sum is: {int(val1) + int(val2)}")
