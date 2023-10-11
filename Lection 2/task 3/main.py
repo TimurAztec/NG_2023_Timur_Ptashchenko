55f585fe6942f156afbfbe3c0f6a1771be82d71e
TimurAztec <timotiaztec@gmail.com>
2023-10-11 16:10:21 +0300

    changed i and j to something more understandable

--- Lection 2/task 3/main.py
+++ Lection 2/task 3/main.py
@@ -1,15 +1,15 @@
 end_num = int(input("Enter the number: "))
-for i in range(1, end_num+1):
+for iterational_num in range(1, end_num+1):
     zero_dividers = set()
-    for j in range(1, i+1):
-        if i%j == 0:
-            zero_dividers.add(j)
-    print(f"{i} | {zero_dividers}")
+    for divider_num in range(1, iterational_num+1):
+        if iterational_num%divider_num == 0:
+            zero_dividers.add(divider_num)
+    print(f"{iterational_num} | {zero_dividers}")
 
 prime_numbers = set()
 prime_numbers.add(2)
 for num in range(3, end_num + 1):
-    if (num % 2 != 0 and all(num % i != 0 for i in range(3, int(num**0.5) + 1, 2))):
+    if (num % 2 != 0 and all(num % prime_num != 0 for prime_num in range(3, int(num**0.5) + 1, 2))):
         prime_numbers.add(num)
 
 print(f"\nPrime numbers:\n {prime_numbers}")
