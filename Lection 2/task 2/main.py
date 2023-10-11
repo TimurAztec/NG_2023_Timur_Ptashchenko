c1e14fdcc557e227a886d64aa282462761b3ead4
TimurAztec <timotiaztec@gmail.com>
2023-10-11 16:05:36 +0300

    Simplified code

--- Lection 2/task 2/main.py
+++ Lection 2/task 2/main.py
@@ -1,11 +1,3 @@
-def is_float_convertable(val):
-    try:
-        float(val)
-    except ValueError:
-        return False
-    else:
-        return True
-
 elements = []
 while True:
     val = input("Enter unique value or enter nothig to finish: ")
@@ -15,7 +7,11 @@ while True:
 
 count = 0
 for item in elements:
-    if is_float_convertable(item):
+    try:
+        float(item)
+    except ValueError:
+        continue
+    else:
         count += 1
 
 print(f"Number of numbers in list: {count} | {elements}")
