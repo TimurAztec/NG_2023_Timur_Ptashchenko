4ba2063f351b47d9a8c823baccaefdb7d8c280c1
TimurAztec <timotiaztec@gmail.com>
2023-10-16 20:44:34 +0300

    Added task 3

--- Lection 3/task 3/main.py
+++ Lection 3/task 3/main.py
@@ -3,16 +3,16 @@ import json
 file_name = input("Enter file name: ")
 key_name = input("Enter key you wanna get: ")
 
-def find_key(data, key):
+def find_key(data, key_name):
     results = []
     if isinstance(data, dict):
-        if key in data:
-            results.append(data[key])
-        for value in data.items():
-            results += find_key(value, key)
+        if key_name in data:
+            results.append(data[key_name])
+        for key, value in data.items():
+            results += find_key(value, key_name)
     elif isinstance(data, list):
         for item in data:
-            results += find_key(item, key)
+            results += find_key(item, key_name)
     return results
 
 
